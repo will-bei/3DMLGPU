4b181a5f8a1d473e33ed1b97882f1a5ad375c5d6
MadMehx <65829543+MadMehx@users.noreply.github.com>
2025-06-11 00:30:31 -0700

    chosenσs

--- run_sjc_torch.py
+++ run_sjc_torch.py
@@ -234,7 +234,7 @@ def sjc_3d(
             
             with record_function("loss_calc"):
                 with torch.no_grad():
-                    chosen_σs = np.random.choice(ts, bs, replace=False)
+                    chosenσs = np.random.choice(ts.cpu().numpy(), bs, replace=False)
                     chosen_σs = chosen_σs.reshape(-1, 1, 1, 1)
                     chosen_σs = torch.as_tensor(chosen_σs, device=model.device, dtype=torch.float32)
                     # chosen_σs = us[i]
